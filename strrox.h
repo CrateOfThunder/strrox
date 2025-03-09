9d2a9916dfff10b2982fe88a73b1a5764df4fa12
CrateOfThunder <xcvi.wgtds.x60@gmail.com>
2025-03-09 00:30:34 -0600

    Create strrox.h

---
+++ strrox.h
@@ -0,0 +1,40 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <https://unlicense.org>
+ */
+
+/* ----------------------------------------------------------------------- */
+/* Bidirectional Bit Shifting Extension for Supporting Strings             */
+/*              strrox.h -- string bit rotation implementation             */
+/* Ver. 1.00                    02AUG2024                   CrateOfThunder */
+/* ----------------------------------------------------------------------- */
+#ifndef _STRROX_H
+#define _STRROX_H
+
+#include <stddef.h>
+
+void strrox(void *v, size_t size, size_t nbits, int shift);
+
+#endif
